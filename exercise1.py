badee293add0e73e5b0ccfcb2fd178cfb7cecd4f
Caden Hong <caden.p.hong@gmail.com>
2022-09-17 12:06:56 -0400

    Added instructions for exercise1

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,20 @@
+'''
+Name Greeting!
+
+Create a function that takes a name and returns a greeting in the form of a string.
+
+Examples
+hello_name("Gerald") ➞ "Hello Gerald!"
+
+hello_name("Tiffany") ➞ "Hello Tiffany!"
+
+hello_name("Ed") ➞ "Hello Ed!"
+Notes
+The input is always a name (as string).
+Don't forget the exclamation mark!
+Enter your script below. Be sure to save a copy to compare with the response on the next screen.
+'''
+
 def hello_name(name):
     greeting = print("Hello " + name + "!")
     return greeting
