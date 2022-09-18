078f7429ea714de4210831b2014b2c484e59150f
Caden Hong <caden.p.hong@gmail.com>
2022-09-18 17:45:22 -0400

    convert min to seconds

---
+++ exercise3.py
@@ -0,0 +1,20 @@
+'''
+Convert Minutes into Seconds
+
+Write a function that takes an integer minutes and converts it to seconds.
+
+Examples:
+convert(5) ➞ 300
+convert(3) ➞ 180
+convert(2) ➞ 120
+
+Notes:
+Don't forget to return the result.
+'''
+
+def convert(min):
+    return min*60
+
+print(convert(5))
+print(convert(3))
+print(convert(2))
