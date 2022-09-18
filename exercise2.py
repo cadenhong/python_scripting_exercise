18ef0095f872dac52244191e4f18482e60f474a1
Caden Hong <caden.p.hong@gmail.com>
2022-09-18 14:45:13 -0400

    return sum of two numbers

---
+++ exercise2.py
@@ -0,0 +1,21 @@
+'''
+Return the Sum of Two Numbers
+
+Create a function that takes two numbers as arguments and returns their sum.
+
+Examples
+addition(3, 2) ➞ 5
+addition(-3, -6) ➞ -9
+addition(7, 3) ➞ 10
+
+Notes
+Don't forget to return the result.
+'''
+
+def addition(num1, num2):
+    sum=num1+num2
+    return sum
+
+print(addition(3, 2))
+print(addition(-3, -6))
+print(addition(7, 3))
