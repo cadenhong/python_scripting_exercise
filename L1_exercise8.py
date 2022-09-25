2ad1e39975167bd7fba344cf89f8068880d98f05
Caden Hong <caden.p.hong@gmail.com>
2022-09-24 22:02:31 -0400

    perimeter of a rectangle

---
+++ L1_exercise8.py
@@ -0,0 +1,20 @@
+'''
+Find the Perimeter of a Rectangle
+
+Create a function that takes length and width and finds the perimeter of a rectangle.
+
+Examples
+find_perimeter(6, 7) ➞ 26
+find_perimeter(20, 10) ➞ 60
+find_perimeter(2, 9) ➞ 22
+
+Notes
+Don't forget to return the result.
+'''
+
+def find_perimeter(length, width):
+    return (length * 2) + (width * 2)
+
+print(find_perimeter(6, 7))
+print(find_perimeter(20, 10))
+print(find_perimeter(2, 9))
