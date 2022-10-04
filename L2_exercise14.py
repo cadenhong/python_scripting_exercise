c40a19df3759a5eee1a0c8e75d9a413e6673f2fb
Caden Hong <caden.p.hong@gmail.com>
2022-10-04 13:21:53 -0400

    find domain name using ip address

---
+++ L2_exercise14.py
@@ -0,0 +1,23 @@
+'''
+Find Domain Name From DNS Pointer (PTR) Records Using IP Address
+
+Write a function that takes an IP address and returns the domain name using PTR DNS records.
+
+Example:
+get_domain("8.8.8.8") ➞ "dns.google"
+get_domain("8.8.4.4") ➞ "dns.google"
+
+Notes:
+- You may want to import socket.
+- Don't cheat and just print the domain name, you need to make a real DNS request.
+- Return as a string.
+'''
+
+import socket
+
+def get_domain(ip):
+    domain_name = socket.gethostbyaddr(ip)[0]
+    return domain_name
+
+print(get_domain("8.8.8.8"))
+print(get_domain("8.8.4.4"))
